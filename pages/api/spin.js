dc8c96fe094dccdebaca11f02c7d4bed9994a060
Celia Tang <celia@MacBook-Pro.local>
2025-03-21 15:50:42 -0400

    added pictures

--- pages/api/spin.js
+++ pages/api/spin.js
@@ -5,30 +5,30 @@ const SHOPIFY_ACCESS_TOKEN = process.env.SHOPIFY_API_KEY;
 
 const PRIZES = [
   // Common Prizes (65%)
-  { name: "Cute Sticker Pack", probability: 0.10, image: "/assets/sticker-pack.jpg" },
-  { name: "Mini Keychain", probability: 0.10, image: "/assets/mini-keychain.jpg" },
-  { name: "Small Plushie", probability: 0.10, image: "/assets/small-plushie.jpg" },
-  { name: "Postcard Set", probability: 0.05, image: "/assets/postcard-set.jpg" },
-  { name: "Acrylic Pin", probability: 0.05, image: "/assets/acrylic-pin.jpg" },
-  { name: "Random Gacha Capsule",  probability: 0.05, image: "/assets/gacha-capsule.jpg" },
-  { name: "Phone Charm", probability: 0.05, image: "/assets/phone-charm.jpg" },
-  { name: "Cute Socks", probability: 0.05, image: "/assets/cute-socks.jpg" },
-  { name: "Sticker Sheet", probability: 0.03, image: "/assets/sticker-sheet.jpg" },
-  { name: "Character Badge", probability: 0.03, image: "/assets/character-badge.jpg" },
-  { name: "Mini Notebook", probability: 0.02, image: "/assets/mini-notebook.jpg" },
-  { name: "Art Print", probability: 0.01, image: "/assets/art-print.jpg" },
-  { name: "Cafe Coupon", probability: 0.01, image: "/assets/cafe-coupon.jpg" },
+  { name: "Cute Sticker Pack", probability: 0.10, image: "https://cdn.shopify.com/s/files/1/0456/6269/9542/files/sproutkunvinylsticker.jpg?v=1741290061" },
+  { name: "Mini Keychain", probability: 0.10, image: "https://cdn.shopify.com/s/files/1/0456/6269/9542/files/charsiubaovinylsticker.jpg?v=1741289925" },
+  { name: "Small Plushie", probability: 0.10, image: "https://cdn.shopify.com/s/files/1/0456/6269/9542/files/siumaivinylsticker.jpg?v=1741289652" },
+  { name: "Postcard Set", probability: 0.05, image: "https://cdn.shopify.com/s/files/1/0456/6269/9542/files/froggymcfrog.jpg?v=1741289235" },
+  { name: "Acrylic Pin", probability: 0.05, image: "https://cdn.shopify.com/s/files/1/0456/6269/9542/files/strawberrymoovinylsticker.jpg?v=1741289068" },
+  { name: "Random Gacha Capsule",  probability: 0.05, image: "https://cdn.shopify.com/s/files/1/0456/6269/9542/files/slothvinylsticker.jpg?v=1741286482" },
+  { name: "Phone Charm", probability: 0.05, image: "https://cdn.shopify.com/s/files/1/0456/6269/9542/files/slothvinylsticker.jpg?v=1741286482" },
+  { name: "Cute Socks", probability: 0.05, image: "https://cdn.shopify.com/s/files/1/0456/6269/9542/files/eggtartbeary.jpg?v=1741286367" },
+  { name: "Sticker Sheet", probability: 0.03, image: "https://cdn.shopify.com/s/files/1/0456/6269/9542/files/mushroomfriendvinylsticker.jpg?v=1741286198" },
+  { name: "Character Badge", probability: 0.03, image: "https://cdn.shopify.com/s/files/1/0456/6269/9542/files/lightcactusvinylsticker.jpg?v=1741286057" },
+  { name: "Mini Notebook", probability: 0.02, image: "https://cdn.shopify.com/s/files/1/0456/6269/9542/files/leafyfriendvinylsticker.jpg?v=1741285751" },
+  { name: "Art Print", probability: 0.01, image: "https://cdn.shopify.com/s/files/1/0456/6269/9542/files/minimushroomsvinylsticker.jpg?v=1741285512" },
+  { name: "Cafe Coupon", probability: 0.01, image: "https://cdn.shopify.com/s/files/1/0456/6269/9542/files/kittybobavinylsticker.jpg?v=1741285376" },
 
   // Rare Prizes (25%)
-  { name: "Deluxe Plushie", probability: 0.07, image: "/assets/deluxe-plushie.jpg" },
-  { name: "Special Edition Pin", probability: 0.06, image: "/assets/special-pin.jpg" },
-  { name: "Holographic Art Print", probability: 0.05, image: "/assets/holo-art-print.jpg" },
-  { name: "Themed Tote Bag", probability: 0.04, image: "/assets/themed-tote.jpg" },
-  { name: "Limited Edition Keychain", probability: 0.03, image: "/assets/limited-keychain.jpg" },
+  { name: "Deluxe Plushie", probability: 0.07, image: "https://cdn.shopify.com/s/files/1/0456/6269/9542/files/umbrellamushroom.jpg?v=1741285187" },
+  { name: "Special Edition Pin", probability: 0.06, image: "https://cdn.shopify.com/s/files/1/0456/6269/9542/files/bearybubblesvinylsticker.jpg?v=1741279636" },
+  { name: "Holographic Art Print", probability: 0.05, image: "https://cdn.shopify.com/s/files/1/0456/6269/9542/files/catpybara.jpg?v=1731641814" },
+  { name: "Themed Tote Bag", probability: 0.04, image: "https://cdn.shopify.com/s/files/1/0456/6269/9542/files/froggycat.jpg?v=1731641748" },
+  { name: "Limited Edition Keychain", probability: 0.03, image: "https://cdn.shopify.com/s/files/1/0456/6269/9542/files/minimcfrog.jpg?v=1731641878" },
 
   // Ultra Rare Prizes (10%)
-  { name: "VIP Cafe Membership", probability: 0.05, image: "/assets/vip-membership.jpg" },
-  { name: "Exclusive Collector’s Box", probability: 0.05, image: "/assets/collectors-box.jpg" }
+  { name: "VIP Cafe Membership", probability: 0.05, image: "https://cdn.shopify.com/s/files/1/0456/6269/9542/files/cutlery1.jpg?v=1731724532" },
+  { name: "Exclusive Collector’s Box", probability: 0.05, image: "https://cdn.shopify.com/s/files/1/0456/6269/9542/files/maruapproved.jpg?v=1731724532" }
 ];
 
 // Function to pick a prize based on weighted probabilities
