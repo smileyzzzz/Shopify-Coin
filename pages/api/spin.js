17ce1ca59561426c4b5bcbe7cbb94643d26b7377
Celia Tang <celia@MacBook-Pro.local>
2025-03-22 11:11:59 -0400

    added GET to CORS control

--- pages/api/spin.js
+++ pages/api/spin.js
@@ -121,7 +121,7 @@ export default async function handler(req, res) {
     // Handle CORS preflight request
   if (req.method === "OPTIONS") {
     res.setHeader("Access-Control-Allow-Origin", "https://cafedeyume.com");
-    res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
     return res.status(200).end();
 
